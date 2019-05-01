1103574f2dac6f08378d932fd1dcba73e5f0d3f5
Sharad Pratap <sh4r4d _at_ _G-mail_>
2019-05-01 21:26:19 +0530

    correction commit

--- schsh.scm
+++ schsh.scm
@@ -1,14 +1,38 @@
 #!/usr/bin/guile -s
 !#
 
-
 ;; https://brennan.io/2015/01/16/write-a-shell-in-c/
 
 
+(define (loop x)
+  x
+  (loop x))
 
 (define (shell_loop)
   (display "hello world")
   (newline))
+
+(define (read-line)
+  (read))
+
+(define (split-line line)
+  line)
+
+(define (execute-line args)
+  args)
+
+(define (shell_loop)
+  (display "prompt> ")
+  (let ((line (read-line)))
+    (when (not
+           (or
+            (equal? line "exit")
+            (eof-object? line)))
+      (let* ((args (split-line line))
+             (status (execute-line args)))
+        status)
+      (newline)
+      (shell_loop))))
 
 
 (define (scsh)
@@ -16,5 +40,7 @@
   (newline)
   (shell_loop))
 
+
+;; (loop (print (read)))
 
 (scsh)
